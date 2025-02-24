9594f558b264308d21dd379c07aba022514e40c2
Informatica <informatica@alumno.local>
2025-02-24 08:17:35 -0600

    CONFIGURACION SERVIDOR Y BASE DE DATOS

---
+++ configs/server.js
@@ -0,0 +1,39 @@
+"use strict"
+
+import express from "express"
+import cors from "cors"
+import helmet from "helmet"
+import morgan from "morgan"
+import {dbConnection} from "./mongo.js"
+//import apiLimiter from "../src/middlewares/validar-cantidad-peticiones.js"
+
+const middlewares = (app) =>{
+    app.use(express.urlencoded({extended: false}))
+    app.use(express.json())
+    app.use(cors())
+    app.use(helmet())
+    app.use(morgan("dev"))
+    //app.use(apiLimiter)
+}
+
+const conectarDB = async () =>{
+    try{
+        await dbConnection()
+    }catch(e){
+        console.log(`DATABASE CONNECTION FAILED: ${e}`)
+        process.exit(1)    
+    }
+}
+
+export const inicioServidor = () =>{
+    const app = express()
+    try{
+        middlewares(app)
+        conectarDB()
+        //routes(app)
+        app.listen(process.env.PORT)
+        console.log(`SERVER CORRIENDO EN EL PUERTO ${process.env.PORT}`)
+    }catch(err){
+        console.log(`FALLO INICIO DEL SERVIDOR: ${err}`) 
+    }
+}
